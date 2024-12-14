d4bf4e5e1e9d966a59732dd07f37109a9994df58
Yash <yashpatidar260707@gmail.com>
2024-12-14 12:29:06 +0530

    Added prime or not program

---
+++ prime.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    bool prime = 1;
+
+    for(int i=2; i<n/2; i++){
+        if(n%i == 0){
+            prime = 0;
+            break;
+        }
+    }
+    if(prime){
+        cout << "number is prime" << endl;
+    } else {
+        cout << "number is not a prime" << endl;
+    }
+    return 0;
+}
