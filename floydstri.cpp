f4ce968563677e64b492ba170013227146060b37
Yash <yashpatidar260707@gmail.com>
2024-12-15 12:28:45 +0530

    updated floyd's triangle pattern to character

--- floydstri.cpp
+++ floydstri.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
     int n = 4;
-    int num = 1;
+    char ch='A';
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<i+1; j++){
-            cout << num << " ";
-            num++;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
